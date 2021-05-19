470f349ccae290465e075efe04a57e4f4098e5e1
jcguu95 <jcguu95@gmail.com>
2021-05-19 10:20:05 +0800

    minor change

--- src/cron-to-lisp.lisp
+++ src/cron-to-lisp.lisp
@@ -234,8 +234,7 @@ control-string into a lispy expression."
                                 (if (eql 1 (length conss))
                                     (cons 'member (cons->integer-sequence (car conss)))
                                     (cons 'or (loop for cons in conss
-                                                    collect (cons 'member (cons->integer-sequence cons))))))
-                              )))
+                                                    collect (cons 'member (cons->integer-sequence cons)))))))))
     (setf result (reduce #'append result))))
 
 ;; TODO write a test
@@ -261,3 +260,6 @@ control-string into a lispy expression."
 ;;  (MEMBER 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26
 ;;          27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49
 ;;          50 51 52 53 54 55 56 57 58 59 60))
+;;
+;; TODO rewrite the macros schedule-lambda and dry-run so that it
+;; can take a proper list.
