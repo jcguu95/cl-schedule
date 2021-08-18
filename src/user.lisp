f65c41781f34763216d1aac02362aa0ad7c1dd27
jcguu95 <jcguu95@gmail.com>
2021-08-17 22:46:55 -0500

    Implemented #'unschedule, #'all-schedules.

--- src/user.lisp
+++ src/user.lisp
@@ -2,6 +2,8 @@
 
 (in-package :cl-schedule)
 
+(defvar *schedules* nil)
+
 (defmacro schedule! (&key name form time)
   "Schedule the FORM to be run with NAME and time spec TIME."
   `(let* (;; create a stateless schedule
@@ -9,12 +11,13 @@
           ;; create a scheduler that remembers the last scheduled time
           (scheduler (make-scheduler schedule)))
      ;; schedule a function as a timer
-     (schedule-function (lambda () ,@form)
-                        scheduler
-                        :name ,name
-                        :immediate t
-                        :ignore-skipped t
-                        :thread t)))
+     (push (schedule-function (lambda () ,@form)
+                               scheduler
+                               :name ,name
+                               :immediate t
+                               :ignore-skipped t
+                               :thread t)
+           *schedules*)))
 
 (defmacro dry-run (n &optional schedule-definition)
   "Dry-run N times for the schedule defined by the
@@ -25,3 +28,10 @@ SCHEDULE-DEFINITION."
          (dotimes (,x ,n)
            (format t "~s~%" (local-time:universal-to-timestamp next-time))
            (setf next-time (next-time schedule :init-time next-time)))))))
+
+;; API with schedules
+;; TODO Native inspection, pause.. etc.
+(defun all-schedules () *schedules*)
+(defun unschedule (schedule)
+  (trivial-timers:unschedule-timer schedule)
+  (setf *schedules* (remove schedule *schedules*)))
