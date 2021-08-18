58df59da050938d072e51fd1b71f21c1ac34ef93
jcguu95 <jcguu95@gmail.com>
2021-08-17 22:18:03 -0500

    Removed fluff and added a docstring.

--- src/user.lisp
+++ src/user.lisp
@@ -3,15 +3,9 @@
 (in-package :cl-schedule)
 
 (defmacro schedule! (&key name form time)
-  ;; Basic example
-  ;;
-  ;; (schedule!
-  ;;  :name "demo"
-  ;;  :form ((print (get-universal-time)))
-  ;;  :time (:second '(member 0 10 20 30 40 50)))
+  "Schedule the FORM to be run with NAME and time spec TIME."
   `(let* (;; create a stateless schedule
-          (schedule (make-typed-cron-schedule
-                     ,@time))
+          (schedule (make-typed-cron-schedule ,@time))
           ;; create a scheduler that remembers the last scheduled time
           (scheduler (make-scheduler schedule)))
      ;; schedule a function as a timer
@@ -25,9 +19,6 @@
 (defmacro dry-run (n &optional schedule-definition)
   "Dry-run N times for the schedule defined by the
 SCHEDULE-DEFINITION."
-  ;; basic example
-  ;;
-  ;; (dry-run 8 (:day-of-week '(member 0 1 2)))
   (let ((x (gensym)))
     `(let ((schedule (make-typed-cron-schedule ,@schedule-definition)))
        (let ((next-time (next-time schedule)))
