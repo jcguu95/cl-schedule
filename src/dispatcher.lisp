97856a8d34b648eccf12cc0836a07a19cc10d742
jcguu95 <jcguu95@gmail.com>
2023-01-29 16:31:02 +0800

    Modularize condition handler.

--- src/dispatcher.lisp
+++ src/dispatcher.lisp
@@ -3,6 +3,20 @@
 (defparameter *dispatcher-name*
   "cl-schedule:global-dispatcher")
 
+(defun wrap-with-simple-condition-handler (function &key extra-info)
+  "Expect FUNCTION to take zero argument. Return the FUNCTION
+wrapped with a condition handler."
+  (lambda ()
+    (handler-case (funcall function)
+      (error (condition)
+        (format t "
+A function raises an unhandled condition:
+  time       :: ~a
+  function   :: ~a
+  extra-info :: ~a
+  condition  :: ~a
+" (local-time:now) function extra-info condition)))))
+
 (defun global-dispatchers ()
   "Return the lists of global dispatchers. We identify them by
 superficially by thread names."
@@ -18,17 +32,19 @@ superficially by thread names."
       (log:debug "Dispatching..")
       (loop for schedule in (gethash now *actions*)
             do (pop (gethash now *actions*))
-               ;; (when (enabled schedule)
-               ;;   (funcall (func schedule)))
                (when (enabled schedule)
-                 (let ((function (func schedule)))
-                   (handler-case (funcall function)
-                     (error (condition)
-                       (format t "~%<CL-SCHEDULE> ~a
-A schedule raises an unhandled condition:
-  schedule: ~a
-  condition: ~a
-" (local-time:now) schedule condition)))))))))
+                 (funcall (wrap-with-simple-condition-handler (func schedule)
+                                                              :extra-info schedule)))
+;;                (when (enabled schedule)
+;;                  (let ((function (func schedule)))
+;;                    (handler-case (funcall function)
+;;                      (error (condition)
+;;                        (format t "~%<CL-SCHEDULE> ~a
+;; A schedule raises an unhandled condition:
+;;   schedule: ~a
+;;   condition: ~a
+;; " (local-time:now) schedule condition)))))
+            ))))
 
 (defun reset-global-dispatcher ()
   (mapc #'bt:destroy-thread (global-dispatchers))
